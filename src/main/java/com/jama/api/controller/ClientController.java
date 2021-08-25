afc9f822f4331514fa9608df1d47c52b1a5e1b99
Dante Ceja <cejadali@hotmail.com>
2021-08-24 23:11:07 -0700

    prueba

--- src/main/java/com/jama/api/controller/ClientController.java
+++ src/main/java/com/jama/api/controller/ClientController.java
@@ -78,7 +78,7 @@ public class ClientController {
 		List<Client> clientList = clientService.getClients();					
 		model.addAttribute("cliente", clientList);
 		if(clientList.isEmpty()) 
-			throw new StudentNotFoundException("No hay clientes registrados. Registre uno para hacer el listado");
+			throw new StudentNotFoundException("No hay clientes registrados.");
 		return clientList;
 	}
 
